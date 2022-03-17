bac38703730c95720c73008a468cc05fb76b6d50
Ezgi Safçı <ezgi.safci@onur.net>
2022-03-17 15:24:17 +0300

    signup-login pages added

--- ezgoShop/src/app/app-routing.module.ts
+++ ezgoShop/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePAgeComponent } from './components/welcome-page/welcome-page.component';
+import { ProductsComponent } from './components/products/products.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {path:'welcomePage', component:WelcomePAgeComponent},
-  {path:'', redirectTo:'welcomePage', pathMatch:'full'}
-
+  {path:'', redirectTo:'welcomePage', pathMatch:'full'},
+  {path:'products', component:ProductsComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent}
 ];
 
 @NgModule({
--- ezgoShop/src/app/app.module.ts
+++ ezgoShop/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePAgeComponent } from './components/welcome-page/welcome-page.component';
@@ -8,7 +7,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SideCartComponent } from './components/side-cart/side-cart.component';
 import { ProductsComponent } from './components/products/products.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,16 @@ import { ProductsComponent } from './components/products/products.component';
     HeaderComponent,
     FooterComponent,
     SideCartComponent,
-    ProductsComponent
+    ProductsComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
