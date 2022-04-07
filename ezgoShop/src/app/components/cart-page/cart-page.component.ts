89de0a3d8432dd35729bce97e2ddc86f7ce8f472
Ezgi Safçı <ezgisafci@outlook.com>
2022-04-07 22:51:05 +0300

    order log test

--- ezgoShop/src/app/components/cart-page/cart-page.component.ts
+++ ezgoShop/src/app/components/cart-page/cart-page.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Cart } from 'src/app/models/cart.model';
 import { CartItem } from 'src/app/models/cartItem.model';
@@ -11,7 +13,8 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class CartPageComponent implements OnInit {
 cart!:Cart;
-  constructor(private cartService :CartService, private router :Router) {
+public orderSave!: any; 
+  constructor(private cartService :CartService, private router :Router, private http :HttpClient) {
     this.setCart();
    }
 
@@ -28,9 +31,27 @@ cart!:Cart;
   }
   setCart(){
     this.cart = this.cartService.getCart();
+    console.log(this.cart.items);
+    // const orderSave = {
+    //   title: [''],
+    //   totalPrice: ''
+    // };
+    // orderSave.totalPrice = (this.cart.totalPrice as any);
+    // this.cart.items[0].product.title; //order json log
   }
+  proceedToPay(){
+
+    // this.http.post<any>("http://localhost:3000/Orders", this.orderSave.value).subscribe(res=>{
+    //   alert('successfull');
+    // },err=>{
+    //   alert('Something went wrong!');
+    // })
+    alert("Pay is proceed!")
+}
+  
 
   ngOnInit(): void {
+    
   }
 
 }
